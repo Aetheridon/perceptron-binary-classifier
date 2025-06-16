533ccc03a7e8ade549d4294eb9d4d154d6a4476e
Aetheridon <lewisvr@protonmail.com>
2025-06-16 13:04:17 +0100

    model can now be trained to modify weights and biases

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+from perceptron import Perceptron
 import pandas as pd
+
+df = pd.read_csv("mock_weather_data.csv")
+df.dropna(inplace=True)
+model = Perceptron(data=df)
+
+percent = 80
+d_split = int((percent / 100) * len(df)) # Trains on X% of data
+model.train(data_split=d_split, learning_rate=0.1, epochs=20)
+prediction = model.hypothesis(.85, .95)
+print(f"Prediction from .85 H and .95 P: {prediction}")
--- src/perceptron.py
+++ src/perceptron.py
@@ -11,9 +11,28 @@ class Perceptron:
         classifier = self.step(score)
         return classifier
     
-    def train(self, data_split):
+    def train(self, data_split, learning_rate=0.01, epochs=10):
+        '''Responsible for caliberating the biases and weights'''
         training_data = self.data[:data_split]
-        # More to come
-    
+        
+        for i in range(epochs):
+            print(f"Training: on Epoch {i}")
+            for x, row in training_data.iterrows():
+                x1 = row["humidity"]
+                x2 = row["pressure"]
+                r_true = row["rain"] # Whether rain happened or not, represented by 1 or 0 respectively.
+                print(f"Training: Training with H: {x1} & P: {x2} with result {r_true}")
+
+                prediction = self.hypothesis(x1, x2)
+                s_value = self.step(prediction)
+                error = r_true - prediction
+                print(f"Training: Predicted with score {prediction} and a step value of {s_value}, error margin is {error}")
+
+                # Update weights and biases based off the error margin
+                self.weight_1 += learning_rate * error * x1
+                self.weight_2 += learning_rate * error * x2
+                self.bias += learning_rate * error # This variable represents how well our model fits the relationship between the data
+                print(f"Training: Weight_1 updated: {self.weight_1}, Weight_2 updated: {self.weight_2}, Bias updated: {self.bias}")
+
     def step(self, score):
         return 1 if score >= 0 else 0
